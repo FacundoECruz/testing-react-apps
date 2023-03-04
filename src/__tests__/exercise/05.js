90197d81b1185b276e8567843b99432e3e070d5b
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-03-04 17:20:49 -0300

    solved use one-off server handlers

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -5,6 +5,7 @@ import * as React from 'react'
 import {render, waitForElementToBeRemoved, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {build, fake} from '@jackfranklin/test-data-bot'
+import {rest} from 'msw'
 import {setupServer} from 'msw/node'
 import {handlers} from 'test/server-handlers'
 import Login from '../../components/login-submission'
@@ -46,5 +47,24 @@ test('omitting the password results in an error', async () => {
   await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
   expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
     `"password required"`,
+  )
+})
+
+test('unknown server error displays the error message', async () => {
+  server.use(
+    rest.post(
+      'https://auth-provider.example.com/api/login',
+      async (req, res, ctx) => {
+        return res(ctx.status(500), ctx.json({message: 'something is wrong'}))
+      },
+    ),
+  )
+  render(<Login />)
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
+    `"something is wrong"`,
   )
 })
